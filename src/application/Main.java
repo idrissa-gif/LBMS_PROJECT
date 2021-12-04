95015d9f86590bc6537ef33cd87b9e877531aab4
Idrissa Mahamoudou <idrissa@iut-dhaka.edu>
2021-12-04 23:59:15 +0600

    first commit

---
+++ src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.*;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLMain.fxml"));
+
+        Scene scene = new Scene(root, 600, 600);
+        
+        primaryStage.setTitle("Library Management System Admin App");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
